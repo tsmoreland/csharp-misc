0bdc45bac056c1b64690df12d2decd81f2ab7da0
Terry Moreland <tsmoreland@live.com>
2020-06-27 13:56:27 +0100

    simplified Union and added intersect
    - reworked Union to use Linq rather than a for loop  manually checking using containskey, instead it unions the keys then creates a dictionary, the key => value handls the check for duplicate entries and performs merging if necessary
    - intersect is only offered using merge function as it by definition will always have conflicts

--- CSharp/Util/Extensions/DictionaryExtensions.cs
+++ CSharp/Util/Extensions/DictionaryExtensions.cs
@@ -14,14 +14,14 @@
 using System;
 using System.Collections.Generic;
 
-namespace Moreland.CSharp.Util.Extensions
+namespace System.Linq
 {
     public static class DictionaryExtensions
     {
         public delegate TValue MergeConfict<TValue>(TValue first, TValue second);
 
         /// <summary>
-        /// Performs a union of two dictionarys using <paramref name="conflictHandler"/> to handle duplicate entries
+        /// Performs a union of two dictionarys using <paramref name="mergeHandler"/> to handle duplicate entries
         /// </summary>
         /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
         /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
@@ -33,46 +33,37 @@ namespace Moreland.CSharp.Util.Extensions
         /// An <see cref="IDictionary{TKey, TValue}"/> whose distinct elements form the 
         /// second set for the union
         /// </param>
-        /// <param name="conflictHandler">
+        /// <param name="mergeHandler">
         /// <see cref="MergeUsingFirst{TValue}(TValue, TValue)"/> used to merge conflicting entries 
         /// between <paramref name="first"/> and <paramref name="second"/>
         /// </param>
         /// <returns>
         /// An <see cref="IDictionary{TKey, TValue}"/> that contains the elements from both
-        /// input sequences, where duplicates are merged using <paramref name="conflictHandler"/>
+        /// input sequences, where duplicates are merged using <paramref name="mergeHandler"/>
         /// </returns>
-        /// <exception cref="ArgumentNullException"><paramref name="first"/>, <paramref name="second"/>, or <paramref name="conflictHandler"/> are <c>null</c></exception>
-        public static IDictionary<TKey, TValue> Union<TKey, TValue>(this IDictionary<TKey, TValue> first, IDictionary<TKey, TValue> second, MergeConfict<TValue> conflictHandler)
+        /// <exception cref="ArgumentNullException"><paramref name="first"/>, <paramref name="second"/>, or <paramref name="mergeHandler"/> are <c>null</c></exception>
+        public static IDictionary<TKey, TValue> Union<TKey, TValue>(this IDictionary<TKey, TValue> first, IDictionary<TKey, TValue> second, MergeConfict<TValue> mergeHandler)
             where TKey : notnull
         {
             if (first == null)
                 throw new ArgumentNullException(nameof(first));
             if (second == null)
                 throw new ArgumentNullException(nameof(second));
-            if (conflictHandler == null)
-                throw new ArgumentNullException(nameof(conflictHandler));
+            if (mergeHandler == null)
+                throw new ArgumentNullException(nameof(mergeHandler));
 
-            var superset = new Dictionary<TKey, TValue>(first);
-
-#           if NETSTANDARD2_0 || NET40 || NET45 || NET451 || NET452 || NET46 || NET461 || NET462 || NET47 || NET471 || NET472 || NET48
-            foreach (var keyValuePair in second)
-            {
-                var key = keyValuePair.Key;
-                var value = keyValuePair.Value;
-                if (!superset.ContainsKey(key))
-                    superset[key] = value;
-                else
-                    superset[key] = conflictHandler(superset[key], value);
-            }
-#           else
-            foreach (var (key, value) in second)
-                if (!superset.ContainsKey(key))
-                    superset[key] = value;
-                else
-                    superset[key] = conflictHandler(superset[key], value);
-#           endif
-
-            return superset;
+            return first.Keys.Union(second.Keys)
+                .ToDictionary(key => key, key =>
+                {
+                    bool firstHasKey = first.ContainsKey(key);
+                    bool secondHasKey = second.ContainsKey(key);
+                    if (firstHasKey && secondHasKey)
+                        return mergeHandler(first[key], second[key]);
+                    else if (firstHasKey)
+                        return first[key];
+                    else
+                        return second[key];
+                });
         }
 
         /// <summary>
@@ -118,5 +109,36 @@ namespace Moreland.CSharp.Util.Extensions
         public static IDictionary<TKey, TValue> UnionMergeUsingSecond<TKey, TValue>(this IDictionary<TKey, TValue> first, IDictionary<TKey, TValue> second)
             where TKey : notnull =>
             Union(first, second, (firstValue, secondValue) => secondValue);
+
+        /// <summary>
+        /// Produces the set intersection of two sequences by using the default equality
+        /// comparer to compare keys.  Values are then merged using <paramref name="mergeHandler"/>
+        /// </summary>
+        /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+        /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
+        /// <param name="first">
+        /// An <see cref="IDictionary{TKey, TValue}"/> whose distinct elements that also
+        /// </param>
+        /// <param name="second">
+        /// </param>
+        /// <param name="mergeHandler">
+        /// <see cref="MergeUsingFirst{TValue}(TValue, TValue)"/> used to merge conflicting entries 
+        /// between <paramref name="first"/> and <paramref name="second"/>
+        /// </param>
+        /// <returns>An <see cref="IDictionary{TKey, TValue}"/> that contains the elements that form the set intersection of two sequences.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="first"/>, <paramref name="second"/>, or <paramref name="conflictHandler"/> are <c>null</c></exception>
+        public static IDictionary<TKey, TValue> Intersect<TKey, TValue>(this IDictionary<TKey, TValue> first, IDictionary<TKey, TValue> second, MergeConfict<TValue> mergeHandler)
+            where TKey : notnull
+        {
+            if (first == null)
+                throw new ArgumentNullException(nameof(first));
+            if (second == null)
+                throw new ArgumentNullException(nameof(second));
+            if (mergeHandler == null)
+                throw new ArgumentNullException(nameof(mergeHandler));
+
+            return first.Keys.Intersect(second.Keys)
+                .ToDictionary(key => key, key => mergeHandler(first[key], second[key]));
+        }
     }
 }
