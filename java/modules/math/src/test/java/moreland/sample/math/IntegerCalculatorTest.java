0419191aebf0990d69bf16ec9c796988ef050b80
Terry Moreland <tsmoreland@live.com>
2020-10-19 21:47:46 +0100

    still toying with getting the tests working with the module

--- java/modules/math/src/test/java/moreland/sample/math/IntegerCalculatorTest.java
+++ java/modules/math/src/test/java/moreland/sample/math/IntegerCalculatorTest.java
@@ -13,18 +13,8 @@
 
 package moreland.sample.math;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.Arrays;
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class IntegerCalculatorTest {
 
