2448219901192b4ab3c6372c054300562295e976
Terry Moreland <tsmoreland@live.com>
2020-10-30 02:35:59 +0000

    added application to test jna messagebox - code in place but not yet tested

---
+++ java/jna/interop-app/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module moreland.sample.jna.interop.app {
+    requires java.base;
+    requires moreland.sample.jna.interop.service;
+
+    uses moreland.sample.jna.interop.service.MessageBox;
+}
---
+++ java/jna/run.cmd
@@ -0,0 +1,1 @@
+java --module-path interop-app\target\interop-app-1.0-SNAPSHOT.jar;interop-win32-ansi\target\interop-win32-ansi-1.0-SNAPSHOT.jar;interop-service\target\interop-service-1.0-SNAPSHOT.jar; -m moreland.sample.jna.interop.app/moreland.sample.jna.interop.app.Application
