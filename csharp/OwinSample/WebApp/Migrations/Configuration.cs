e3dbcceab7a903ac87d3e42804145f9e40032ff2
Terry Moreland <tsmoreland@live.com>
2020-12-15 00:55:30 +0000

    minor updates - cosmetic tweaks - db migration (initial)

---
+++ csharp/OwinSample/WebApp/Migrations/Configuration.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity.Migrations;
+
+namespace OwinSample.WebApp.Migrations
+{
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Infrastructure.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(OwinSample.WebApp.Infrastructure.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
