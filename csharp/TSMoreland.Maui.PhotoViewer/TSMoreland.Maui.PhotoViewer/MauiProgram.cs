85eba5064f55d2fddd93c5634a8637ffecd6d09c
Terry Moreland <tsmoreland@live.com>
2021-11-15 00:08:09 +0000

    sample  maui app - first impression is simply - it's not ready, lots of exceptions on startup when using Windows, maybe it works better for Android/iOS but no very usable on Windows at present

---
+++ csharp/TSMoreland.Maui.PhotoViewer/TSMoreland.Maui.PhotoViewer/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Hosting;
+
+namespace TSMoreland.Maui.PhotoViewer
+{
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                })
+                .UseMauiApp<App>()
+                .Host
+                .ConfigureAppConfiguration((app, config) =>
+                    config
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddUserSecrets<App>()
+                        .AddEnvironmentVariables());
+
+			return builder.Build();
+		}
+	}
+}
