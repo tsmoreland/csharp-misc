fdcbf343d1ee2df5baafc1727454e19180541c74
Terry Moreland <tsmoreland@live.com>
2021-12-06 01:09:12 +0000

    code clean up + shift of options to dbcontext

--- TSMoreland.ObjectTracker/TSMoreland.ObjectTracker.App/Program.cs
+++ TSMoreland.ObjectTracker/TSMoreland.ObjectTracker.App/Program.cs
@@ -3,25 +3,22 @@ using Microsoft.EntityFrameworkCore;
 using TSMoreland.ObjectTracker.Data;
 using TSMoreland.ObjectTracker.Data.Abstractions;
 using TSMoreland.ObjectTracker.Data.Abstractions.Entities;
+using TSMoreland.ObjectTracker.Data.Abstractions.ViewModels;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var services = builder.Services;
+IServiceCollection services = builder.Services;
 services.AddAuthorization();
-services
-    .AddDbContext<ObjectContext>(options => 
-    { 
-        options.UseSqlite("Data Source=objectTracker.db", b => 
-            b.MigrationsAssembly(typeof(ObjectContext).Assembly.FullName));
-    });
+services.AddDbContext<ObjectContext>();
+
 services.AddScoped<IObjectRepository, ObjectRepository>();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
+using (IServiceScope scope = app.Services.CreateScope())
 {
-    var db = scope.ServiceProvider.GetRequiredService<ObjectContext>();
+    ObjectContext db = scope.ServiceProvider.GetRequiredService<ObjectContext>();
     db.Database.Migrate();
 }
 
@@ -32,6 +29,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -44,7 +42,7 @@ app.MapGet("/error", () => Results.Problem());
 
 app.MapPost("/objects", async ([FromServices] IObjectRepository repository, ObjectEntity entity, CancellationToken cancellationToken) =>
 {
-    var createdEntity = await repository.Add(entity, cancellationToken);
+    ObjectEntity createdEntity = await repository.Add(entity, cancellationToken);
     await repository.Commit(cancellationToken);
     return Results.Created($"/objects/{createdEntity.Id}", createdEntity);
 });
@@ -56,7 +54,7 @@ app.MapGet("/objects",
 app.MapGet("/objects/{id:int}",
     async ([FromServices] IObjectRepository repository, [FromRoute] int id, CancellationToken cancellationToken) =>
     {
-        var result = await repository.GetById(id, cancellationToken);
+        ObjectViewModel? result = await repository.GetById(id, cancellationToken);
         return result is not null
             ? Results.Ok(result)
             : Results.NotFound();
@@ -64,7 +62,7 @@ app.MapGet("/objects/{id:int}",
 
 app.MapPost("/objects/{id:int}/logs", async ([FromServices] IObjectRepository repository, [FromRoute] int id, LogEntity entity, CancellationToken cancellationToken) =>
 {
-    var createdEntity = await repository.AddMessage(id, entity, cancellationToken);
+    LogEntity createdEntity = await repository.AddMessage(id, entity, cancellationToken);
     await repository.Commit(cancellationToken);
     return Results.Created($"/objects/{id}/logs/{createdEntity.Id}", createdEntity);
 });
--- TSMoreland.ObjectTracker/TSMoreland.ObjectTracker.Data/ObjectContext.cs
+++ TSMoreland.ObjectTracker/TSMoreland.ObjectTracker.Data/ObjectContext.cs
@@ -13,8 +13,18 @@ public sealed class ObjectContext : DbContext
     public DbSet<ObjectEntity> Objects { get; set; } = null!; // initialization will be handled by EF
 
     /// <inheritdoc/>
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=objectTracker.db", b => 
+            b.MigrationsAssembly(typeof(ObjectContext).Assembly.FullName));
+
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    /// <inheritdoc/>
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // table configuration for fluent design goes here, we don't need any yet as convention is enough
     }
 
 }
