27cb80dc23a3d20525232efbb396a2860c79a622
Terry Moreland <tsmoreland@live.com>
2021-12-19 00:06:57 +0000

    updated application to query name and attempt to get id from COM - currently seeing ArgumentException when attempting to get GUID property, string is fine

--- TSMoreland.Interop/TSMoreland.Interop.App/Program.cs
+++ TSMoreland.Interop/TSMoreland.Interop.App/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.Versioning;
+
+if (OperatingSystem.IsWindows())
+{
+    Console.WriteLine("Application not supported on any platform other than Windows");
+}
+
+Guid classId = new ("E3D3572D-9E25-4CF3-82F5-45B6F0035A82");
+Type? classType = GetClassTypeFromId(classId);
+if (classType == null)
+{
+    Console.WriteLine($"Class {classId} not found");
+    return;
+}
+object? instance = Activator.CreateInstance(classType);
+if (instance == null)
+{
+    Console.WriteLine($"Class {classType} returned null on creation");
+    return;
+}
+
+dynamic dynamicInstance = instance;
+
+string name = dynamicInstance.Name;
+Console.WriteLine($"Name = {name}");
+
+Guid temp;
+object result = dynamicInstance.get_Id(out temp);
+
+Guid id = dynamicInstance.Id;
+Console.WriteLine($"Id = {id}");
+
+Console.Out.WriteLine($"{id} {name}");
+
+
+static Type? GetClassTypeFromId(Guid classId)
+{
+    if (OperatingSystem.IsWindows())
+    {
+        return GetWindowsClassTypeFromId(classId);
+    }
+
+    throw new NotSupportedException();
+}
+
+[SupportedOSPlatform("Windows")]
+static Type? GetWindowsClassTypeFromId(Guid classId)
+{
+    return Type.GetTypeFromCLSID(classId);
+}
