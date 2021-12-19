7e29a6de137822a32375054dfe98ae01710fcf8a
Terry Moreland <tsmoreland@live.com>
2021-12-19 13:25:44 +0000

    sample netframework app to generate interop and compare with net6 version

---
+++ TSMoreland.Interop/TSMoreland.Interop.NetFramework.App/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TSMoreland.Interop.NetFramework.App;
+
+internal static class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        SimpleInProcessCOMLib.ISimpleObject instance = new SimpleInProcessCOMLib.SimpleObject();
+
+        instance.Numeric = 37;
+        (Guid id, string name, int numeric) = (instance.Id, instance.Name, instance.Numeric);
+
+        Console.WriteLine($"Id = {id}, name = {name} numeric = {numeric}");
+    }
+
+    private static Type GetClassTypeFromId(Guid classId)
+    {
+        return Type.GetTypeFromCLSID(classId);
+    }
+
+}
