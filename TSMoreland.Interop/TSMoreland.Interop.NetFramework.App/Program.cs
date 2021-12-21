58ca99c67ee98ea097849e055573691428bae80e
Terry Moreland <tsmoreland@live.com>
2021-12-21 00:57:56 +0000

    correct net framework app, no longer using prefer 32-bit

--- TSMoreland.Interop/TSMoreland.Interop.NetFramework.App/Program.cs
+++ TSMoreland.Interop/TSMoreland.Interop.NetFramework.App/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SimpleInProcessCOMLib;
 
 namespace TSMoreland.Interop.NetFramework.App;
 
@@ -7,12 +8,15 @@ internal static class Program
     [STAThread]
     public static void Main(string[] args)
     {
-        SimpleInProcessCOMLib.ISimpleObject instance = new SimpleInProcessCOMLib.SimpleObject();
+        ISimpleObject instance = new SimpleObjectClass();
 
         instance.Numeric = 37;
         (Guid id, string name, int numeric) = (instance.Id, instance.Name, instance.Numeric);
 
         Console.WriteLine($"Id = {id}, name = {name} numeric = {numeric}");
+
+        string result = instance.ConvertToString(Guid.Empty);
+        Console.WriteLine(result);
     }
 
     private static Type GetClassTypeFromId(Guid classId)
