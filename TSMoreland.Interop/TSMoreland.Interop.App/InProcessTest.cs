f5281c4052568bdf598fe8efa66c0774530378e8
Terry Moreland <tsmoreland@live.com>
2021-12-26 00:22:27 +0000

    re-org in process test splitting code out into smaller functions

--- TSMoreland.Interop/TSMoreland.Interop.App/InProcessTest.cs
+++ TSMoreland.Interop/TSMoreland.Interop.App/InProcessTest.cs
@@ -21,28 +21,67 @@ internal static class InProcessTest
 
     public static void Verify()
     {
-        Guid classId = new("E3D3572D-9E25-4CF3-82F5-45B6F0035A82");
-        Type? classType = TypeHelper.GetClassTypeFromId(classId);
-
-
-        if (classType == null)
+        if (!TryCreateComObject(out object? @object))
         {
-            Console.WriteLine($"Class {classId} not found");
             return;
         }
 
-        object? @object = Activator.CreateInstance(classType);
-        if (@object == null)
-        {
-            Console.WriteLine($"Class {classType} returned null on creation");
-            return;
-        }
-
-        dynamic instance = @object;
+        dynamic instance = @object!;
 
         string name = instance.Name;
         Console.WriteLine($"Name = {name}");
 
+        instance.Numeric = 24;
+        int numeric = instance.Numeric;
+        Console.WriteLine($"Numeric = {numeric}");
+
+        MinimalEventTest(@object!);
+
+        ConnectionPointContainerTest(instance);
+
+        string description = instance.Description;
+        Console.WriteLine(description);
+
+        // Beginning of Out of Process, should move elsewhere 
+
+        SimpleObjectTest(@object!);
+        MinimalSimpleObjectTest(@object!);
+        TryAccessGuidMethod(instance);
+
+        DispatchTest(@object!);
+
+
+        Guid id = instance.Id;
+        Console.WriteLine($"Id = {id}");
+
+        Console.Out.WriteLine($"{id} {name}");
+    }
+
+    private static bool TryCreateComObject(out object? @object)
+    {
+        Guid classId = new("E3D3572D-9E25-4CF3-82F5-45B6F0035A82");
+        Type? classType = TypeHelper.GetClassTypeFromId(classId);
+
+        @object = null;
+
+        if (classType == null)
+        {
+            Console.WriteLine($"Class {classId} not found");
+            return false;
+        }
+
+        @object = Activator.CreateInstance(classType);
+        if (@object == null)
+        {
+            Console.WriteLine($"Class {classType} returned null on creation");
+            return false;
+        }
+
+        return true;
+    }
+
+    private static void ConnectionPointContainerTest(dynamic instance)
+    {
         if (instance is IConnectionPointContainer container)
         {
             container.EnumConnectionPoints(out IEnumConnectionPoints connectionPoints);
@@ -77,9 +116,6 @@ internal static class InProcessTest
                 }
             }
         }
-
-
-
         _ISimpleObjectEvents_OnPropertyChangedEventHandler callbackDelegate =
             new(InProcessInstance_OnPropertyChanged);
 
@@ -133,42 +169,10 @@ internal static class InProcessTest
         }
 
 
-        string description = instance.Description;
-        Console.WriteLine(description);
+    }
 
-        // Beginning of Out of Process, should move elsewhere 
-
-
-        if (@object is ISimpleObject simpleObject)
-        {
-            dynamic dynamicSimpleObject = simpleObject;
-
-            try
-            {
-                // theory here is that even though we started with an ISimpleObject we can
-                // still access ISimpleObject2 when using a dynamic
-                string dynamicDescription = dynamicSimpleObject.Description;
-                Console.WriteLine(dynamicDescription);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-
-
-            Guid instanceId = simpleObject.Id;
-            Console.WriteLine($"Id from manually implemented C# interface {instanceId}");
-            string converted = simpleObject.ConvertToString(Guid.Empty);
-
-            Console.WriteLine($"Converted {converted}");
-        }
-
-        if (@object is IMinimalSimpleObject simpleObject2)
-        {
-            Guid instanceId = simpleObject2.Id;
-            Console.WriteLine($"Id from manually implemented C# interface {instanceId}");
-        }
-
+    private static void TryAccessGuidMethod(dynamic instance)
+    {
         try
         {
             string converted = instance.ConvertToString(Guid.Empty);
@@ -179,63 +183,114 @@ internal static class InProcessTest
             Console.WriteLine(ex.Message);
         }
 
+    }
 
-        if (@object is IDispatch dispatch)
+    private static void SimpleObjectTest(object @object)
+    {
+        if (@object is not ISimpleObject simpleObject)
         {
-            Guid empty = Guid.Empty;
+            return;
+        }
 
-            string[] properties = new[] { "Id" };
-            foreach (string property in properties)
-            {
-                string propertyName = property;
+        dynamic dynamicSimpleObject = simpleObject;
 
-                int result = dispatch.GetDispId(ref empty, ref propertyName, 1, 2048, out int dispId);
-
-                if (result == 0)
-                {
-                    Console.WriteLine($"Disp Id: {dispId}");
-
-                    DISPPARAMS dispParams = new()
-                    {
-                        cArgs = 0, cNamedArgs = 0, rgdispidNamedArgs = IntPtr.Zero, rgvarg = IntPtr.Zero,
-                    };
-                    EXCEPINFO excepInfo = new();
-
-                    try
-                    {
-                        result = dispatch.Invoke(dispId, ref empty, 2048, (ushort)INVOKEKIND.INVOKE_PROPERTYGET,
-                            ref dispParams,
-                            out object variant, ref excepInfo,
-                            out uint argErr);
-
-                        if (result == 0)
-                        {
-                            Console.WriteLine($"Success: {variant}");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Result = {result:X}");
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"Result = {result}");
-                }
-            }
-
+        try
+        {
+            // theory here is that even though we started with an ISimpleObject we can
+            // still access ISimpleObject2 when using a dynamic
+            string dynamicDescription = dynamicSimpleObject.Description;
+            Console.WriteLine(dynamicDescription);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
         }
 
 
-        Guid id = instance.Id;
-        Console.WriteLine($"Id = {id}");
+        Guid instanceId = simpleObject.Id;
+        Console.WriteLine($"Id from manually implemented C# interface {instanceId}");
+        string converted = simpleObject.ConvertToString(Guid.Empty);
 
-        Console.Out.WriteLine($"{id} {name}");
+        Console.WriteLine($"Converted {converted}");
+
     }
+
+    private static void MinimalSimpleObjectTest(object @object)
+    {
+        if (@object is not IMinimalSimpleObject simpleObject2)
+        {
+            return;
+        }
+
+        Guid instanceId = simpleObject2.Id;
+        Console.WriteLine($"Id from manually implemented C# interface {instanceId}");
+
+    }
+
+    private static void MinimalEventTest(object @object)
+    {
+        if (@object is not ISimpleObjectEventsEvent eventProducer)
+        {
+            Console.WriteLine("object does not implement event producer or interface is incorrect");
+            return;
+        }
+
+    }
+
+    private static void DispatchTest(object @object)
+    {
+        if (@object is not IDispatch dispatch)
+        {
+            return;
+        }
+
+        Guid empty = Guid.Empty;
+
+        string[] properties = new[] { "Id" };
+        foreach (string property in properties)
+        {
+            string propertyName = property;
+
+            int result = dispatch.GetDispId(ref empty, ref propertyName, 1, 2048, out int dispId);
+
+            if (result == 0)
+            {
+                Console.WriteLine($"Disp Id: {dispId}");
+
+                DISPPARAMS dispParams = new()
+                {
+                    cArgs = 0, cNamedArgs = 0, rgdispidNamedArgs = IntPtr.Zero, rgvarg = IntPtr.Zero,
+                };
+                EXCEPINFO excepInfo = new();
+
+                try
+                {
+                    result = dispatch.Invoke(dispId, ref empty, 2048, (ushort)INVOKEKIND.INVOKE_PROPERTYGET,
+                        ref dispParams,
+                        out object variant, ref excepInfo,
+                        out uint argErr);
+
+                    if (result == 0)
+                    {
+                        Console.WriteLine($"Success: {variant}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Result = {result:X}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Result = {result}");
+            }
+        }
+    }
+
 
     private static void InProcessInstance_OnPropertyChanged(string propertyName)
     {
