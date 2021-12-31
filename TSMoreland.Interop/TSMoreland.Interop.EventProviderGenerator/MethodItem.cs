e1ad67fbdf613fea35c5558aa4e836257aa5b66e
Terry Moreland <tsmoreland@live.com>
2021-12-31 00:56:30 +0000

    method item semi-dto - also handles string formatting

---
+++ TSMoreland.Interop/TSMoreland.Interop.EventProviderGenerator/MethodItem.cs
@@ -0,0 +1,39 @@
+﻿//
+// Copyright © 2021 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System.Collections.Immutable;
+
+namespace TSMoreland.Interop.EventProviderGenerator;
+
+internal record struct MethodItem(
+    int DispId,
+    string Name,
+    string Type,
+    ImmutableArray<ParameterItem> Parameters)
+{
+    /// <inheritdoc />
+    public override string ToString()
+    {
+
+        if (!Parameters.Any())
+        {
+            return $"{Type} {Name}();";
+        }
+
+        string parameters = Parameters
+            .Select(p => p.ToString())
+            .Aggregate((a, b) => $"{a}, {b}");
+        return $"{Type} {Name}({parameters});";
+
+    }
+}
