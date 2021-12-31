7ecf582b757ee0cfa53959e1282a242ea1847067
Terry Moreland <tsmoreland@live.com>
2021-12-31 00:57:14 +0000

    adding IsExternalInit to allow use of records in netstandard2.0 project

---
+++ TSMoreland.Interop/TSMoreland.Interop.EventProviderGenerator/IsExternalInit.cs
@@ -0,0 +1,11 @@
+﻿#if !NET5_0_OR_GREATER
+
+using System.ComponentModel;
+
+// ReSharper disable once CheckNamespace -- specific namespace required, needed to suppres warning due to use of init in netstandard2.0
+namespace System.Runtime.CompilerServices;
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+internal class IsExternalInit{}
+
+#endif
