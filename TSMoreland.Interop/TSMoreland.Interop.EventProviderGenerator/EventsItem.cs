c39fae4f9f0d9d479ddbed10f34be7613b6f3e39
Terry Moreland <tsmoreland@live.com>
2022-01-02 00:34:01 +0000

    updated event item - added implementation for build event sink which makes use of new methods in methoditem to construct the helper

--- TSMoreland.Interop/TSMoreland.Interop.EventProviderGenerator/EventsItem.cs
+++ TSMoreland.Interop/TSMoreland.Interop.EventProviderGenerator/EventsItem.cs
@@ -12,15 +12,60 @@
 //
 
 using System.Collections.Immutable;
+using System.Text;
 
 namespace TSMoreland.Interop.EventProviderGenerator;
 
-internal record struct EventsItem(string ClassName, string Namespace, ImmutableArray<MethodItem> Methods)
+internal record struct EventsItem(string InterfaceName, string Namespace, ImmutableArray<MethodItem> Methods)
 {
 
-    public string BuildEventsInterface()
+    public string BuildEventDelegates()
     {
-        throw new NotImplementedException();
+        StringBuilder builder = new();
+        AppendUsings(builder);
+
+        builder
+            .AppendLine($@"namespace {Namespace}")
+            .AppendLine("{");
+
+        foreach (MethodItem method in Methods)
+        {
+            builder.AppendLine($"{method.Delegate}");
+        }
+
+        builder.AppendLine("}");
+
+        return builder.ToString();
+    }
+
+    public string BuildEventsBridgeInterface()
+    {
+        StringBuilder builder = new();
+
+        AppendUsings(builder);
+        builder.AppendLine($@"
+namespace {Namespace}
+{{
+    [TypeLibType(16)]
+    [ComEventInterface(typeof({InterfaceName}), typeof({EventProviderName}))]
+    [ComVisible(false)]
+    public interface {BridgeName}
+    {{");
+        foreach (MethodItem method in Methods)
+        {
+            builder.AppendLine($@"
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        void add_{method.Name}([In] {method.DelegateName} handler);
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        void remove_{method.Name}([In] {method.DelegateName} handler);");
+        }
+
+        builder
+            .AppendLine("    }")
+            .AppendLine("}");
+
+
+        return builder.ToString();
     }
 
     public string BuildEventProvider()
@@ -30,6 +75,46 @@ internal record struct EventsItem(string ClassName, string Namespace, ImmutableA
 
     public string BuildEventSink()
     {
-        throw new NotImplementedException();
+        StringBuilder builder = new();
+
+        AppendUsings(builder);
+        builder
+            .AppendLine($"namespace {Namespace}")
+            .AppendLine("{")
+            .AppendLine($"    internal sealed class {SinkHelperName} : {InterfaceName}")
+            .AppendLine("    {")
+            .AppendLine("        public int Cookie { get; set; }")
+            .AppendLine();
+        foreach (MethodItem method in Methods)
+        {
+            method.AppendDelegateImplementation(builder);
+            method.AppendEventBridgeImplementation(builder);
+        }
+
+        builder
+            .AppendLine("   }")
+            .AppendLine("}");
+
+
+        return builder.ToString();
     }
+
+    private string BridgeName => $"{InterfaceName}Bridge";
+    private string EventProviderName =>
+         InterfaceName.TrimStart('I') + "Provider";
+
+    private string SinkHelperName =>
+         InterfaceName.TrimStart('I') + "SinkHelper";
+
+    private static void AppendUsings(StringBuilder builder)
+    {
+        builder.AppendLine(@"
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+
+#nullable enable
+");
+    }
+
 }
