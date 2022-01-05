65bdb0d08803cb072808bb13d18e77beb5a957a3
Terry Moreland <tsmoreland@live.com>
2022-01-05 20:51:25 +0000

    added OOP object facade with event support (that isn't yet working)

---
+++ TSMoreland.Interop/TSMoreland.Interop.SimpleObjectCOMProxy/ISimpleOOPObjectEvents.cs
@@ -0,0 +1,30 @@
+﻿//
+// Copyright © 2022 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using TSMoreland.Interop.EventProviderGenerator.Abstractions;
+
+namespace TSMoreland.Interop.SimpleObjectCOMProxy;
+
+[TypeLibType(4096)]
+[Guid("8363FCC0-80D3-4968-82CB-C2121D1DF9B5")]
+[InterfaceType(2)]
+[ComImport]
+[ComEventProvider("8363FCC0-80D3-4968-82CB-C2121D1DF9B5")]
+public interface ISimpleOOPObjectEvents
+{
+    [DispId(1)]
+    [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+    void OOPOnPropertyChanged([MarshalAs(UnmanagedType.BStr), In] string propertyName);
+}
