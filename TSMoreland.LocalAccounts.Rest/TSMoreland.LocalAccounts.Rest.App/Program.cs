8ab6681c2b59c8a75a09529fe1060e0a3a4d8c79
Terry Moreland <tsmoreland@live.com>
2022-01-13 00:07:00 +0000

    add serilog to the pipeline

--- TSMoreland.LocalAccounts.Rest/TSMoreland.LocalAccounts.Rest.App/Program.cs
+++ TSMoreland.LocalAccounts.Rest/TSMoreland.LocalAccounts.Rest.App/Program.cs
@@ -1,14 +1,14 @@
+using Serilog;
 using TSMoreland.LocalAccounts.Rest.Infrastructure;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.Host
-    .ConfigureWebHostDefaults(webHostBuilder =>
-        webHostBuilder
-            .UseKestrel(kestrelOptions => kestrelOptions.AddServerHeader = false));
-// use serilog, ...
+builder.WebHost
+    .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration))
+    .UseKestrel(kestrelOptions => kestrelOptions.AddServerHeader = false);
 
 // Add services to the container.
+builder.Services.AddControllers();
 builder.Services.AddAuthorization();
 builder.Services.AddInfrastructure();
 
@@ -18,7 +18,7 @@ WebApplication app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/api/Error");
+    app.UseExceptionHandler("/api/error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -35,7 +35,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapGet("/api/Error", () => Results.Problem());
+app.UseStatusCodePagesWithReExecute("/api/error", "?statusCode={0}");
+app.MapGet("/api/error", (int? statusCode) => Results.Problem(statusCode: statusCode));
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
