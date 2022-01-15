869f09d057c5fd4b9cf9d43c018a9be0907dc5fa
Terry Moreland <tsmoreland@live.com>
2022-01-15 00:42:11 +0000

    new migration used to verify that I can still add one, just adding index for name

---
+++ TSMoreland.ObjectTracker/TSMoreland.ObjectTracker.Data/Migrations/20220115004045_NameIndex.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TSMoreland.ObjectTracker.Data.Migrations
+{
+    public partial class NameIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Objects_Name",
+                table: "Objects",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Objects_Name",
+                table: "Objects");
+        }
+    }
+}
