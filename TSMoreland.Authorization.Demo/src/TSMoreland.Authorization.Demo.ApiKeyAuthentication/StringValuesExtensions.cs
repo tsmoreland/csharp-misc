6f632673b5495cd2eb0b626cd3f2798f3ea2bcf1
Terry Moreland <tsmoreland@live.com>
2022-02-08 22:43:42 +0000

    adding string value extension to get api key value from header value - not much to this one, it'll be the first value we can get

---
+++ TSMoreland.Authorization.Demo/src/TSMoreland.Authorization.Demo.ApiKeyAuthentication/StringValuesExtensions.cs
@@ -0,0 +1,28 @@
+﻿//
+// Copyright © 2022 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Primitives;
+using TSMoreland.Authorization.Demo.Authentication.Abstractions;
+
+namespace TSMoreland.Authorization.Demo.ApiKeyAuthentication;
+
+internal static class StringValuesExtensions
+{
+    public static string GetApiKeyOrThrow(this in StringValues values)
+    {
+        return values.FirstOrDefault() ??
+               throw new AuthenticationFailedException(AuthenticateResult.Fail("missing api key"));
+
+    }
+}
