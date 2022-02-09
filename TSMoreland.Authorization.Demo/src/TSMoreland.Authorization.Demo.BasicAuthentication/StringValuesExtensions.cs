dd7c100eef2b76161dfd713ef7a337a2da1f1442
Terry Moreland <tsmoreland@live.com>
2022-02-09 01:17:53 +0000

    added implementation for string values extensions used by basic auth

--- TSMoreland.Authorization.Demo/src/TSMoreland.Authorization.Demo.BasicAuthentication/StringValuesExtensions.cs
+++ TSMoreland.Authorization.Demo/src/TSMoreland.Authorization.Demo.BasicAuthentication/StringValuesExtensions.cs
@@ -11,7 +11,11 @@
 // WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
+using System.Buffers;
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Primitives;
+using TSMoreland.Authorization.Demo.Authentication.Abstractions;
 
 namespace TSMoreland.Authorization.Demo.BasicAuthentication;
 
@@ -19,7 +23,69 @@ internal static class StringValuesExtensions
 {
     public static (string username, string password) GetBasicUsernameAndPasswordOrThrow(this in StringValues values)
     {
-        throw new NotImplementedException();
+        string encodedAuth = values.FirstOrDefault() ??
+                             throw new AuthenticationFailedException(AuthenticateResult.Fail("missing api key"));
+
+        return encodedAuth.Length > 1024
+            ? DecodeWithPoolAllocatedBufferOrThrow(encodedAuth)
+            : DecodeWithStackAllocatedBufferOrThrow(encodedAuth);
+    }
+
+    private static (string Username, string Password) DecodeWithPoolAllocatedBufferOrThrow(string encoded)
+    {
+        byte[]? pooledBuffer = null;
+        try
+        {
+            pooledBuffer = ArrayPool<byte>.Shared.Rent(encoded.Length);
+            Span<byte> buffer = new(pooledBuffer);
+            if (!Convert.TryFromBase64String(encoded, buffer, out int length))
+            {
+                 throw new AuthenticationFailedException(AuthenticateResult.Fail("authorization is not base64 encoded"));
+            }
+            buffer = buffer[..length];
+            return DecocdeOrThrow(buffer);
+
+        }
+        finally
+        {
+            if (pooledBuffer is not null)
+            {
+                ArrayPool<byte>.Shared.Return(pooledBuffer);
+            }
+        }
+
+
+
+    }
+
+    private static (string Username, string Password) DecodeWithStackAllocatedBufferOrThrow(string encoded)
+    {
+        Span<byte> buffer = stackalloc byte[encoded.Length];
+        if (!Convert.TryFromBase64String(encoded, buffer, out int length))
+        {
+             throw new AuthenticationFailedException(AuthenticateResult.Fail("authorization is not base64 encoded"));
+        }
+        buffer = buffer[..length];
+        return DecocdeOrThrow(buffer);
+    }
+
+    private static (string Username, string Password) DecocdeOrThrow(in Span<byte> decoded)
+    {
+        int index = decoded.IndexOf(':');
+        if (index == -1)
+        {
+            throw new AuthenticationFailedException(AuthenticateResult.Fail("malformed authorization value"));
+        }
+
+        Span<byte> rawUser = decoded[..index];
+        index++;
+        if (++index >= decoded.Length)
+        {
+            return (Encoding.UTF8.GetString(rawUser), string.Empty);
+        }
+        Span<byte> rawPassword = decoded[index..];
+
+        return (Encoding.UTF8.GetString(rawUser), Encoding.UTF8.GetString(rawPassword));
     }
 
 }
