0e5a9ad76da12fbc7d0c985358eb8cf16b4b7183
Terry Moreland <tsmoreland@live.com>
2022-02-13 00:15:51 +0000

    new migration for local users database

---
+++ TSMoreland.Authorization.Demo/src/TSMoreland.Authorization.Demo.LocalUsers/Migrations/20220212235727_DemoApiKeyNormalizedName.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TSMoreland.Authorization.Demo.LocalUsers.Migrations
+{
+    public partial class DemoApiKeyNormalizedName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedName",
+                table: "DemoApiKeys",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NormalizedName",
+                table: "DemoApiKeys");
+        }
+    }
+}
