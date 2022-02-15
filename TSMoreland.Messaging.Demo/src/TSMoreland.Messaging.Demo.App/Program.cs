34f7773241278b09b74443383058bda4de3705fa
Terry Moreland <tsmoreland@live.com>
2022-02-15 23:20:28 +0000

    main app, initial impl based on very basic sample

---
+++ TSMoreland.Messaging.Demo/src/TSMoreland.Messaging.Demo.App/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using TSMoreland.Messaging.Demo.App;
+using IMsHost = Microsoft.Extensions.Hosting.IHost;
+
+IHostBuilder builder = Host.CreateDefaultBuilder();
+builder
+    .ConfigureLogging(configureLogging =>
+        configureLogging
+            .SetMinimumLevel(LogLevel.Information)
+            .AddDebug()
+            .AddConsole());
+builder.ConfigureServices((hostContext, services) =>
+{
+    services
+        .AddMassTransit(configureTransit =>
+        {
+            configureTransit.AddConsumer<MessageConsumer>();
+            configureTransit.UsingInMemory((busRegistrationContext, factoryConfigurator) =>
+            {
+                factoryConfigurator.ConfigureEndpoints(busRegistrationContext);
+            });
+
+        })
+        .AddMassTransitHostedService()
+        .AddHostedService<WorkerService>();
+});
+
+IMsHost host = builder.Build();
+
+host.Run();
