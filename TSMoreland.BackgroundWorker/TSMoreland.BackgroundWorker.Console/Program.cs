46b704ed7c1c3504a99d5e8b6329189234e15d7a
Terry Moreland <tsmoreland@live.com>
2022-06-26 21:28:25 +0100

    background worker example - not sure what else to call it - the app uses a background service along with a message that includes a task completion source to allow a psuedo async call but to a single 'thread'

---
+++ TSMoreland.BackgroundWorker/TSMoreland.BackgroundWorker.Console/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Channels;
+using TSMoreland.BackgroundWorker.Console;
+
+Console.WriteLine("Hello, World!");
+
+string line;
+
+
+
+Channel<Message> channel = Channel.CreateBounded<Message>(new BoundedChannelOptions(10));
+Worker worker = new(channel);
+
+CancellationTokenSource cancelSource = new();
+await worker.StartAsync(cancelSource.Token);
+
+do
+{
+    line = Console.ReadLine()?.ToUpperInvariant() ?? string.Empty;
+
+    if (!int.TryParse(line, out int value))
+    {
+        break;
+    }
+
+    Message msg = new(value, new TaskCompletionSource<int>());
+    await channel.Writer.WriteAsync(msg);
+
+    int result = await msg.CompletionSource.Task;
+    Console.WriteLine(result);
+
+} while (line != "QUIT");
+
+CancellationTokenSource stopSource = new();
+await worker.StopAsync(stopSource.Token);
+
