92f35aad87f43eb862fc7f364d18145e1c995f9a
Terry Moreland <tsmoreland@live.com>
2022-07-18 00:29:43 +0100

    certificate generator

---
+++ TSMoreland.AspNetCore.AuthSamples/TSMoreland.AspNetCore.AuthSample.CertificateAuth.Consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿//
+// Copyright © 2022 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System.Security.Cryptography.X509Certificates;
+
+string root = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", ".."));
+
+string certificatePemFile = Path.Combine(root, "certificate.cer");
+string certificateKeyFile = Path.Combine(root, "certificate.key");
+
+Console.Write("Certificate password:");
+Console.Out.Flush();
+string? password = Console.ReadLine();
+
+X509Certificate2 certificate = X509Certificate2.CreateFromEncryptedPemFile(
+    certificatePemFile,
+    password,
+    certificateKeyFile);
+
+using HttpClientHandler handler = new();
+handler.ClientCertificates.Add(certificate);
+
+using HttpClient client = new(handler, false);
+// see CertificateAuth.App launch settings
+client.BaseAddress = new Uri("https://localhost:7140");
+
+
