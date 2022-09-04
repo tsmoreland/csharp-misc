6dc4df22c8d33d52a531383450afe7f0e3042358
Terry Moreland <tsmoreland@live.com>
2022-09-04 01:34:14 +0100

    package updates + add HttpGet attribute the view page methods

--- HttpsRedirect/HttpsRedirectCore/Controllers/HomeController.cs
+++ HttpsRedirect/HttpsRedirectCore/Controllers/HomeController.cs
@@ -13,16 +13,19 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    [HttpGet]
     public IActionResult Index()
     {
         return View();
     }
 
+    [HttpGet]
     public IActionResult Privacy()
     {
         return View();
     }
 
+    [HttpGet]
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
--- HttpsRedirect/HttpsRedirectNet/Controllers/HomeController.cs
+++ HttpsRedirect/HttpsRedirectNet/Controllers/HomeController.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace HttpsRedirect.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +18,7 @@ namespace HttpsRedirect.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
