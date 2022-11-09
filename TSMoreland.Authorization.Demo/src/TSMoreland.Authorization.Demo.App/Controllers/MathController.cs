5ff8d5dc6ee44552fcaa1db832a3129c4b0bb856
Terry Moreland <tsmoreland@live.com>
2022-11-09 21:46:11 +0000

    add ignoreantiforgery - should be a no-op, mostly to see if snyk stops raising a concern for a webapi

--- TSMoreland.Authorization.Demo/src/TSMoreland.Authorization.Demo.App/Controllers/MathController.cs
+++ TSMoreland.Authorization.Demo/src/TSMoreland.Authorization.Demo.App/Controllers/MathController.cs
@@ -26,6 +26,7 @@ public class MathController : ControllerBase
     public record IntegerMathInputDto([Required] int X, [Required] int Y);
 
     [HttpPost]
+    [IgnoreAntiforgeryToken]
     [Produces(typeof(IntegerMathResult))]
     [Consumes(typeof(IntegerMathResult), MediaTypeNames.Application.Json)]
     [Route("add")]
